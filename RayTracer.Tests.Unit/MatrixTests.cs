ddcbe3f9a792b5fad189a9271f36b76cb2e98a9a
mik-ke <mickster@duck.com>
2023-09-17 12:18:01 +0300

    Matrix init, git repo author settings test

---
+++ RayTracer.Tests.Unit/MatrixTests.cs
@@ -0,0 +1,19 @@
+﻿using RayTracer.Models;
+using Xunit;
+namespace RayTracer.Tests.Unit;
+
+public class MatrixTests
+{
+	[Fact]
+	public void Constructor_ShouldInitializeCorrectly_WhenCalled()
+	{
+		// Arrange
+		var m = new Matrix(3, 5);
+
+		// Act
+
+
+		// Assert
+
+	}
+}
---
+++ RayTracer/Models/Matrix.cs
@@ -0,0 +1,18 @@
+﻿namespace RayTracer.Models;
+
+public class Matrix
+{
+    #region fields
+    double[,] _matrix;
+    #endregion
+
+    /// <summary>
+    /// Creates a <paramref name="numRows"/> x <paramref name="numColumns"/> matrix data structure.
+    /// </summary>
+    public Matrix(int numRows, int numColumns)
+    {
+        _matrix = new double[numRows, numColumns];
+    }
+
+
+}
