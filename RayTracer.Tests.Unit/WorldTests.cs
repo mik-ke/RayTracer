01d0a41a6d5c8c0ea235969b1fe1e8a7662701d6
mik-ke <mickster@duck.com>
2023-09-27 20:38:34 +0300

    Started creating World object (WIP)

---
+++ RayTracer.Tests.Unit/WorldTests.cs
@@ -0,0 +1,66 @@
+﻿using RayTracer.Models;
+using Xunit;
+using Xunit.Sdk;
+
+namespace RayTracer.Tests.Unit;
+
+public class WorldTests
+{
+    /// <summary>
+    /// Returns a default world for various tests.
+    /// Contains a light source at (-10, 10, -10)
+    /// and two concentric spheres where the outermost is a unit sphere
+    /// and the innnermost has a radius of 0.5. Both lie at the origin.
+    /// </summary>
+	public static World DefaultWorld()
+	{
+        PointLight light = new(new Point(-10, 10, -10), new Color(1, 1, 1));
+        Sphere sphere1 = new();
+        sphere1.Material.Color = new(0.8, 1.0, 0.6);
+        sphere1.Material.Diffuse = 0.7;
+        sphere1.Material.Specular = 0.2;
+        Sphere sphere2 = new();
+        sphere2.Transform = Matrix.Scaling(0.5, 0.5, 0.5);
+
+        World world = new() { LightSource = light };
+        world.Objects.Add(sphere1);
+        world.Objects.Add(sphere2);
+
+        return world;
+	}
+
+	[Fact]
+	public void Constructor_ShouldInitializeEmptyWorld()
+	{
+		// Arrange
+		// Act
+		World world = new();
+
+        // Assert
+        Assert.Empty(world.Objects);
+        Assert.Null(world.LightSource);
+	}
+
+    [Fact]
+    public void Intersect_ShouldReturnCorrectTValues_WhenCalledWithDefaultWorld()
+    {
+        // Arrange
+        World world = DefaultWorld();
+        Ray ray = new(new Point(0, 0, -5), new Vector(0, 0, 1));
+        const int expectedLength = 4;
+        const double t1 = 4;
+        const double t2 = 4.5;
+        const double t3 = 5.5;
+        const double t4 = 6;
+
+        // Act
+        Intersections result = world.Intersect(ray);
+
+        // Assert
+        Assert.Equal(expectedLength, result.Length);
+        Assert.Equal(t1, result[0].T);
+        Assert.Equal(t2, result[1].T);
+        Assert.Equal(t3, result[2].T);
+        Assert.Equal(t4, result[3].T);
+    }
+}
---
+++ RayTracer/Models/World.cs
@@ -0,0 +1,26 @@
+﻿namespace RayTracer.Models;
+
+public sealed class World
+{
+    #region properties
+    public PointLight? LightSource { get; init; } 
+    public List<Sphere> Objects { get; } = new List<Sphere>();
+    #endregion
+
+    /// <summary>
+    /// Returns a collection of t values where the given <paramref name="ray"/> intersects
+    /// with the <see cref="Objects"/>
+    /// </summary>
+    public Intersections Intersect(Ray ray)
+    {
+        var intersections = GetAllIntersections(ray).ToArray();
+        return new Intersections(intersections);
+    }
+
+    private IEnumerable<Intersection> GetAllIntersections(Ray ray)
+    {
+        foreach (var obj in Objects)
+            foreach (var intersection in obj.Intersect(ray))
+                yield return intersection;
+    }
+}
