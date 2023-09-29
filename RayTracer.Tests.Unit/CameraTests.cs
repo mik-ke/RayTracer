1d0e9f876afd578a274057c58cd0a61b472bd405
mik-ke <mickster@duck.com>
2023-09-29 18:57:14 +0300

    Created camera

---
+++ RayTracer.Tests.Unit/CameraTests.cs
@@ -0,0 +1,142 @@
+﻿using RayTracer.Extensions;
+using RayTracer.Models;
+
+namespace RayTracer.Tests.Unit;
+
+public class CameraTests
+{
+	[Fact]
+	public void Constructor_ShouldInitializeCorrectly()
+	{
+		// Arrange
+		const int horizontalSize = 160;
+		const int verticalSize = 120;
+		const double fieldOfView = Math.PI / 2;
+		Matrix expectedTransform = Matrix.Identity(4);
+
+		// Act
+		var result = new Camera(horizontalSize, verticalSize, fieldOfView);
+
+		// Assert
+		Assert.Equal(horizontalSize, result.HorizontalSize);
+		Assert.Equal(verticalSize, result.VerticalSize);
+		Assert.Equal(fieldOfView, result.FieldOfView);
+		Assert.Equal(expectedTransform, result.Transform);
+	}
+
+	[Fact]
+	public void Constructor_ShouldInitializeCorrectly_WhenTransformGiven()
+	{
+		// Arrange
+		const int horizontalSize = 160;
+		const int verticalSize = 120;
+		const double fieldOfView = Math.PI / 2;
+		Matrix transform = Matrix.RotationY(Math.PI / 4) * Matrix.Translation(0, -2, 5);
+
+		// Act
+		var result = new Camera(horizontalSize, verticalSize, fieldOfView, transform);
+
+		// Assert
+		Assert.Equal(horizontalSize, result.HorizontalSize);
+		Assert.Equal(verticalSize, result.VerticalSize);
+		Assert.Equal(fieldOfView, result.FieldOfView);
+		Assert.Equal(transform, result.Transform);
+	}
+
+	[Fact]
+	public void PixelSize_ShouldBeCorrect_WhenCanvasHorizontal()
+	{
+		// Arrange
+		Camera camera = new(200, 125, Math.PI / 2);
+		const double expected = 0.01;
+
+		// Act
+		var actual = camera.PixelSize;
+
+		// Assert
+		Assert.True(expected.IsEqualTo(actual));
+	}
+
+	[Fact]
+	public void PixelSize_ShouldBeCorrect_WhenCanvasVertical()
+	{
+		// Arrange
+		Camera camera = new(125, 200, Math.PI / 2);
+		const double expected = 0.01;
+
+		// Act
+		var actual = camera.PixelSize;
+
+		// Assert
+		Assert.True(expected.IsEqualTo(actual));
+	}
+
+	[Fact]
+	public void RayForPixel_ShouldWork_WhenGoingThroughCanvasCenter()
+	{
+		// Arrange
+		Camera camera = new(201, 101, Math.PI / 2);
+		Point expectedOrigin = new(0, 0, 0);
+		Vector expectedDirection = new(0, 0, -1);
+
+		// Act
+		var result = camera.RayForPixel(100, 50);
+
+		// Assert
+		Assert.Equal(expectedOrigin, result.Origin);
+		Assert.Equal(expectedDirection, result.Direction);
+	}
+
+	[Fact]
+	public void RayForPixel_ShouldWork_WhenGoingThroughCanvasCorner()
+	{
+		// Arrange
+		Camera camera = new(201, 101, Math.PI / 2);
+		Point expectedOrigin = new(0, 0, 0);
+		Vector expectedDirection = new(0.66519, 0.33259, -0.66851);
+
+		// Act
+		var result = camera.RayForPixel(0, 0);
+
+		// Assert
+		Assert.Equal(expectedOrigin, result.Origin);
+		Assert.Equal(expectedDirection, result.Direction);
+	}
+
+	[Fact]
+	public void RayForPixel_ShouldWork_WhenCameraTransformed()
+	{
+		// Arrange
+		Matrix transform = Matrix.RotationY(Math.PI / 4) * Matrix.Translation(0, -2, 5);
+		Camera camera = new(201, 101, Math.PI / 2, transform);
+		Point expectedOrigin = new(0, 2, -5);
+		Vector expectedDirection = new(Math.Sqrt(2) / 2, 0, -Math.Sqrt(2) / 2);
+
+		// Act
+		var result = camera.RayForPixel(100, 50);
+
+		// Assert
+		Assert.Equal(expectedOrigin, result.Origin);
+		Assert.Equal(expectedDirection, result.Direction);
+	}
+
+	// nonrigorous test to show how render ought to work
+	[Fact]
+	public void Render_ShouldRenderWorldCorrectly()
+	{
+		// Arrange
+		World world = WorldTests.DefaultTestWorld();
+		Point from = new(0, 0, -5);
+		Point to = new(0, 0, 0);
+		Vector up = new(0, 1, 0);
+		Matrix transform = Matrix.View(from, to, up);
+		Camera camera = new(11, 11, Math.PI / 2, transform);
+		Color expectedColor = new(0.38066, 0.47583, 0.2855);
+
+		// Act
+		var result = camera.Render(world);
+
+		// Assert
+		Assert.Equal(expectedColor, result.PixelAt(5, 5));
+	}
+}
---
+++ RayTracer/Models/Camera.cs
@@ -0,0 +1,113 @@
+﻿namespace RayTracer.Models;
+
+public sealed class Camera
+{
+    #region properties
+    /// <summary>
+    /// Horizontal size (in pixels) of the canvas the picture will be rendered to.
+    /// </summary>
+    public int HorizontalSize { get; set; }
+
+    /// <summary>
+    /// Vertical size (in pixels) of the canvas the picture will be rendered to.
+    /// </summary>
+    public int VerticalSize { get; set; }
+
+    /// <summary>
+    /// The angle that describes how much the <see cref="Camera"/> can see.
+    /// </summary>
+    public double FieldOfView { get; set; }
+
+    /// <summary>
+    /// Matrix describing how the world is oriented relative to the <see cref="Camera"/>.
+    /// </summary>
+    public Matrix Transform { get; set; }
+
+    /// <summary>
+    /// The size of a single pixel in world space.
+    /// </summary>
+    public double PixelSize { get; private set; }
+
+    /// <summary>
+    /// Half the width of the canvas in world space.
+    /// </summary>
+    public double HalfWidth { get; private set; }
+
+    /// <summary>
+    /// Half the height of the canvas in world space.
+    /// </summary>
+    public double HalfHeight { get; private set; }
+    #endregion
+
+    public Camera(int horizontalSize, int verticalSize, double fieldOfView, Matrix? transform = null)
+    {
+        HorizontalSize = horizontalSize;
+        VerticalSize = verticalSize;
+        FieldOfView = fieldOfView;
+        Transform = transform ?? Matrix.Identity(4);
+        CalculatePixelSize();
+    }
+
+    /// <summary>
+    /// Initializes <see cref="PixelSize"/>, <see cref="HalfWidth"/> and <see cref="HalfHeight"/>.
+    /// </summary>
+    private void CalculatePixelSize()
+    {
+        double halfView = Math.Tan(FieldOfView / 2);
+        double aspect = (double)HorizontalSize / VerticalSize;
+        if (aspect >= 1)
+        {
+            HalfWidth = halfView;
+            HalfHeight = halfView / aspect;
+        }
+        else
+        {
+            HalfWidth = halfView * aspect;
+            HalfHeight = halfView;
+        }
+
+        PixelSize = (HalfWidth * 2) / HorizontalSize;
+    }
+
+    /// <summary>
+    /// Calculates the world coordinates at the center of the given pixel
+    /// and constructs a <see cref="Ray"/> that passes through that point
+    /// </summary>
+    /// <returns>A new <see cref="Ray"/> that passes through the pixel center</returns>
+    public Ray RayForPixel(int pixelX, int pixelY)
+    {
+        // the offset from the edge of the canvas to the pixel's center
+        double xOffset = (pixelX + 0.5) * PixelSize;
+        double yOffset = (pixelY + 0.5) * PixelSize;
+
+        double worldX = HalfWidth - xOffset;
+        double worldY = HalfHeight - yOffset;
+
+        var pixel = (Point)(Transform.Inverse() * new Point(worldX, worldY, -1));
+        var origin = (Point)(Transform.Inverse() * new Point(0, 0, 0));
+        var direction = (pixel - origin).Normalize();
+
+        return new Ray(origin, direction);
+    }
+
+    /// <summary>
+    /// Renders the given <paramref name="world"/>.
+    /// </summary>
+    /// <returns>A new <see cref="Canvas"/>.</returns>
+    public Canvas Render(World world)
+    {
+        Canvas image = new(HorizontalSize, VerticalSize);
+
+        for (int y = 0 ; y < VerticalSize; y++)
+        {
+            for (int x = 0 ; x < HorizontalSize; x++)
+            {
+                Ray ray = RayForPixel(x, y);
+                Color color = world.ColorAt(ray);
+                image[x, y] = color;
+            }
+        }
+
+        return image;
+    }
+}
