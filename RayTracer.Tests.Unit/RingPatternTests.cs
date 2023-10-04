2e85bbc89451591717f502343a68537b28ab7d9a
mik-ke <mickster@duck.com>
2023-10-04 20:43:21 +0300

    Added RingPattern

---
+++ RayTracer.Tests.Unit/RingPatternTests.cs
@@ -0,0 +1,29 @@
+﻿using RayTracer.Models;
+using RayTracer.Patterns;
+
+namespace RayTracer.Tests.Unit;
+
+public class RingPatternTests
+{
+	[Theory]
+	[MemberData(nameof(PatternAtData))]
+	public void PatternAt_ShouldExtendInBothXAndZ(Point point, Color expected)
+	{
+		// Arrange
+		RingPattern pattern = new(Color.White, Color.Black);
+
+		// Act
+		var actual = pattern.PatternAt(point);
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
+	public static IEnumerable<object[]> PatternAtData =>
+		new List<object[]> {
+            new object[] { new Point(0, 0, 0), Color.White },
+            new object[] { new Point(1, 0, 0), Color.Black },
+            new object[] { new Point(0, 0, 1), Color.Black },
+			// 0.708 = slightly more than sqrt(2) / 2
+            new object[] { new Point(0.708, 0, 0.708), Color.Black }
+		};
+}
---
+++ RayTracer/Patterns/RingPattern.cs
@@ -0,0 +1,26 @@
+﻿using RayTracer.Models;
+
+namespace RayTracer.Patterns;
+
+/// <summary>
+/// A ring pattern. Depends on two dimensions x and z.
+/// </summary>
+public class RingPattern : Pattern
+{
+    public Color A { get; }
+    public Color B { get; }
+
+    public RingPattern(Color a, Color b, Matrix? transform = null) : base(transform)
+    {
+        A = a;
+        B = b;
+    }
+
+    public override Color PatternAt(Point point)
+    {
+        var distance = Math.Sqrt(point.X * point.X + point.Z * point.Z);
+
+        if (distance % 2 == 0) return A;
+        return B;
+    }
+}
