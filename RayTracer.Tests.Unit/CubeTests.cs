b73412576250d21d0387db3a27b7453d2434f885
mik-ke <mickster@duck.com>
2023-10-08 16:18:03 +0300

    Added Cube shape

---
+++ RayTracer.Tests.Unit/CubeTests.cs
@@ -0,0 +1,86 @@
+﻿using RayTracer.Models;
+using RayTracer.Shapes;
+
+namespace RayTracer.Tests.Unit;
+
+public class CubeTests
+{
+	[Theory]
+	[MemberData(nameof(IntersectData))]
+	public void Intersect_ShouldBeCorrect_WhenRayFromAnyFace(Point origin, Vector direction,
+		double expectedT1, double expectedT2)
+	{
+		// Arrange
+		Cube cube = new();
+		Ray ray = new(origin, direction);
+
+		// Act
+		var result = cube.Intersect(ray);
+
+		// Assert
+		Assert.Equal(expectedT1, result[0].T);
+		Assert.Equal(expectedT2, result[1].T);
+	}
+	public static IEnumerable<object[]> IntersectData =>
+		new List<object[]>
+		{
+			new object[] { new Point(5, 0.5, 0), new Vector(-1, 0, 0), 4.0, 6.0 },
+			new object[] { new Point(-5, 0.5, 0), new Vector(1, 0, 0), 4.0, 6.0 },
+			new object[] { new Point(0.5, 5, 0), new Vector(0, -1, 0), 4.0, 6.0 },
+			new object[] { new Point(0.5, -5, 0), new Vector(0, 1, 0), 4.0, 6.0 },
+			new object[] { new Point(0.5, 0, 5), new Vector(0, 0, -1), 4.0, 6.0 },
+			new object[] { new Point(0.5, 0, -5), new Vector(0, 0, 1), 4.0, 6.0 },
+			new object[] { new Point(0, 0.5, 0), new Vector(0, 0, 1), -1.0, 1.0 },
+		};
+
+	[Theory]
+	[MemberData(nameof(IntersectMissData))]
+	public void Intersect_ShouldReturnEmpty_WhenRayMissesCube(Point origin, Vector destination)
+	{
+		// Arrange
+		Cube cube = new();
+		Ray ray = new(origin, destination);
+
+		// Act
+		var result = cube.Intersect(ray);
+
+		// Assert
+		Assert.Empty(result);
+	}
+	public static IEnumerable<object[]> IntersectMissData =>
+		new List<object[]>
+		{
+			new object[] { new Point(-2, 0, 0), new Vector(0.2673, 0.5345, 0.8018) },
+			new object[] { new Point(0, -2, 0), new Vector(0.8018, 0.2673, 0.5345) },
+			new object[] { new Point(0, 0, -2), new Vector(0.5345, 0.8018, 0.2673) },
+			new object[] { new Point(2, 0, 2), new Vector(0, 0, 1) },
+			new object[] { new Point(0, 2, 2), new Vector(0, -1, 0) },
+			new object[] { new Point(2, 2, 0), new Vector(-1, 0, 0) },
+		};
+
+	[Theory]
+	[MemberData(nameof(NormalData))]
+	public void Normal_ShouldBeCorrect_WhenAnyPlane(Point intersectionPoint, Vector expected)
+	{
+		// Arrange
+		Cube cube = new();
+
+		// Act
+		var actual = cube.Normal(intersectionPoint);
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
+	public static IEnumerable<object[]> NormalData =>
+		new List<object[]>
+		{
+			new object[] { new Point(1, 0.5, 0.8), new Vector(1, 0, 0) },
+			new object[] { new Point(-1, 0.2, 0.9), new Vector(-1, 0, 0) },
+			new object[] { new Point(-0.4, 1, -0.1), new Vector(0, 1, 0) },
+			new object[] { new Point(0.3, -1, -0.7), new Vector(0, -1, 0) },
+			new object[] { new Point(-0.6, 0.3, 1), new Vector(0, 0, 1) },
+			new object[] { new Point(0.4, 0.4, -1), new Vector(0, 0, -1) },
+			new object[] { new Point(1, 1, 1), new Vector(1, 0, 0) },
+			new object[] { new Point(-1, -1, -1), new Vector(-1, 0, 0) },
+		};
+}
---
+++ RayTracer/Shapes/Cube.cs
@@ -0,0 +1,74 @@
+﻿using RayTracer.Extensions;
+using RayTracer.Models;
+
+namespace RayTracer.Shapes;
+
+/// <summary>
+/// A cube shape. An axis-aligned bounding box (before transformation).
+/// </summary>
+public class Cube : Shape
+{
+    public Cube() : this(null) { }
+    public Cube(Matrix? transform) : base(transform) { }
+
+    protected override Vector LocalNormal(Point localPoint)
+    {
+        var xAbs = Math.Abs(localPoint.X);
+        var yAbs = Math.Abs(localPoint.Y);
+        var zAbs = Math.Abs(localPoint.Z);
+
+        var maxComponent = Math.Max(xAbs, Math.Max(yAbs, zAbs));
+
+        if (maxComponent == xAbs)
+            return new Vector(localPoint.X, 0, 0);
+        else if (maxComponent == yAbs)
+            return new Vector(0, localPoint.Y, 0);
+
+        return new Vector(0, 0, localPoint.Z);
+    }
+
+    protected override Intersections LocalIntersect(Ray localRay)
+    {
+        var (tMinX, tMaxX) = CheckAxis(localRay.Origin.X, localRay.Direction.X);
+        var (tMinY, tMaxY) = CheckAxis(localRay.Origin.Y, localRay.Direction.Y);
+        var (tMinZ, tMaxZ) = CheckAxis(localRay.Origin.Z, localRay.Direction.Z);
+
+        var tMin = Math.Max(tMinX, Math.Max(tMinY, tMinZ));
+        var tMax = Math.Min(tMaxX, Math.Min(tMaxY, tMaxZ));
+
+        if (tMin > tMax)
+            return Intersections.Empty;
+
+        return new Intersections(
+            new Intersection(tMin, this),
+            new Intersection(tMax, this));
+    }
+
+    /// <summary>
+    /// Checks where the ray intersects the corresponding plane.
+    /// Returns the minimum and maximum t values.
+    /// </summary>
+    private static (double tMin, double tMax) CheckAxis(double origin, double direction)
+    {
+        double tMin, tMax;
+
+        var tMinNumerator = -1 - origin;
+        var tMaxNumerator = 1 - origin;
+
+        if (Math.Abs(direction) >= DoubleExtensions.EPSILON)
+        {
+            tMin = tMinNumerator / direction;
+            tMax = tMaxNumerator / direction;
+        }
+        else
+        {
+            tMin = tMinNumerator * double.PositiveInfinity;
+            tMax = tMaxNumerator * double.PositiveInfinity;
+        }
+
+        if (tMin > tMax)
+            return (tMax, tMin);
+
+        return (tMin, tMax);
+    }
+}
