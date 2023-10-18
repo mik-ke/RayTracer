fc51efff845d43e0f8d1ac2f69d858783bb727b0
mik-ke <mickster@duck.com>
2023-10-18 21:35:21 +0300

    Started obj parser (WIP)

---
+++ RayTracer.Tests.Unit/ObjParserTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+namespace RayTracer.Tests.Unit;
+
+public class ObjParserTests
+{
+	[Fact]
+	public void Method_Condition_Expectation()
+	{
+		// Arrange
+
+
+		// Act
+
+
+		// Assert
+
+	}
+}
---
+++ RayTracer/Parsers/ObjParser.cs
@@ -0,0 +1,27 @@
+﻿
+using RayTracer.Shapes;
+
+namespace RayTracer.Parsers;
+
+/// <summary>
+/// Parser for the Wavefront OBJ file format.
+/// </summary>
+public class ObjParser
+{
+    /// <summary>
+    /// Parses the OBJ-formatted <paramref name="objData"/> data into a <see cref="Shapes.Group"/>.
+    /// </summary>
+    public async Task<Group> Parse(string objData)
+    {
+        Group group = new();
+        using (StringReader reader = new StringReader(objData))
+        {
+            string? currentLine;
+            while ((currentLine = await reader.ReadLineAsync()) != null)
+            {
+
+            }
+        }
+        return null!;
+    }
+}
