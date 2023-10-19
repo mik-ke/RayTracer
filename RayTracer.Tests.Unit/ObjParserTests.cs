4de804fbf16b3ce574a5f313b5e351b4f30bd808
mik-ke <mickster@duck.com>
2023-10-19 21:44:53 +0300

    obj parser continued

--- RayTracer.Tests.Unit/ObjParserTests.cs
+++ RayTracer.Tests.Unit/ObjParserTests.cs
@@ -1,18 +1,54 @@
-﻿using Xunit;
+﻿using RayTracer.Models;
+using RayTracer.Parsers;
+
 namespace RayTracer.Tests.Unit;
 
-public class ObjParserTests
+public class ObjTests
 {
 	[Fact]
-	public void Method_Condition_Expectation()
+	public async Task LoadFromStringAsync_ShouldNotThrow_WhenObjStringGibberish()
 	{
 		// Arrange
-
+		const string objData =
+@"
+There was a young lady named Bright
+who traveled much faster than light.
+She set out one day
+in a relative way,
+and came back the previous night.
+";
+		Obj obj = new();
 
 		// Act
+		// Assert
+		await obj.LoadFromStringAsync(objData);
+	}
 
+	[Fact]
+	public async void LoadFromStringAsync_ShouldSetVerticesCorrectly()
+	{
+		// Arrange
+		const string objData =
+@"
+v -1 1 0
+v -1.0000 0.5000 0.0000
+v 1 0 0
+v 1 1 0
+";
+		Obj obj = new();
+
+		Point expectedVertice1 = new(-1, 1, 0);
+		Point expectedVertice2 = new(-1, 0.5, 0);
+		Point expectedVertice3 = new(1, 0, 0);
+		Point expectedVertice4 = new(1, 1, 0);
+
+		// Act
+		await obj.LoadFromStringAsync(objData);
 
 		// Assert
-
+		Assert.Equal(expectedVertice1, obj.Vertices[0]);
+		Assert.Equal(expectedVertice2, obj.Vertices[1]);
+		Assert.Equal(expectedVertice3, obj.Vertices[2]);
+		Assert.Equal(expectedVertice4, obj.Vertices[3]);
 	}
 }
--- RayTracer/Parsers/ObjParser.cs
+++ RayTracer/Parsers/ObjParser.cs
@@ -1,27 +1,65 @@
 ﻿
-using RayTracer.Shapes;
+using RayTracer.Models;
+using System.Globalization;
 
 namespace RayTracer.Parsers;
 
 /// <summary>
-/// Parser for the Wavefront OBJ file format.
+/// Class that represents the information of the Wavefront OBJ file format.
+/// <see cref="LoadFromStringAsync(string)"/> should be called to initialize the object.
 /// </summary>
-public class ObjParser
+public class Obj
 {
+    public List<Point> Vertices;
+
     /// <summary>
-    /// Parses the OBJ-formatted <paramref name="objData"/> data into a <see cref="Shapes.Group"/>.
+    /// Parses the OBJ-formatted <paramref name="objData"/> data and initializes the <see cref="Obj"/>.
     /// </summary>
-    public async Task<Group> Parse(string objData)
+    public async Task LoadFromStringAsync(string objData)
     {
-        Group group = new();
+        Vertices = new();
+
         using (StringReader reader = new StringReader(objData))
         {
             string? currentLine;
             while ((currentLine = await reader.ReadLineAsync()) != null)
             {
-
+                ProcessLine(currentLine);
             }
         }
-        return null!;
+    }
+
+    private void ProcessLine(in string line)
+    {
+        string[] arguments = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        if (arguments.Length == 0) return;
+
+        switch (arguments[0])
+        {
+            case "v":
+                ProcessVertice(arguments);
+                break;
+        }
+    }
+
+    private void ProcessVertice(in string[] arguments)
+    {
+        if (arguments.Length != 4) return;
+
+        if (!double.TryParse(arguments[1],
+            NumberStyles.Any,
+            CultureInfo.InvariantCulture,
+            out double x)) return;
+        if (!double.TryParse(arguments[2],
+            NumberStyles.Any,
+            CultureInfo.InvariantCulture,
+            out double y)) return;
+        if (!double.TryParse(arguments[3],
+            NumberStyles.Any,
+            CultureInfo.InvariantCulture,
+            out double z)) return;
+
+        Point vertice = new(x, y, z);
+        Vertices.Add(vertice);
     }
 }
