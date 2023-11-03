565c8c080bd562780581368838e2fdf5efd5d298
mik-ke <mickster@duck.com>
2023-11-03 18:29:14 +0200

    Started CSG shape implementation

---
+++ RayTracer.Tests.Unit/CSGTests.cs
@@ -0,0 +1,24 @@
+﻿using RayTracer.Shapes;
+using Xunit;
+
+namespace RayTracer.Tests.Unit;
+
+public class CSGTests
+{
+	[Fact]
+	public void Constructor_ShouldInitializeCorrectly()
+	{
+		// Arrange
+		Sphere left = new();
+		Cube right = new();
+		Operation operation = Operation.Union;
+
+		// Act
+		var result = new CSG(operation, left, right);
+
+		// Assert
+		Assert.Equal(left, result.Left);
+		Assert.Equal(right, result.Right);
+		Assert.Equal(operation, result.Operation);
+	}
+}
---
+++ RayTracer/Shapes/CSG.cs
@@ -0,0 +1,42 @@
+﻿using RayTracer.Models;
+
+namespace RayTracer.Shapes;
+
+/// <summary>
+/// Constructive solid geometry. Combines two <see cref="Shape"/>s via set operations.
+/// </summary>
+public sealed class CSG : Shape
+{
+    public Shape Left { get; init; }
+    public Shape Right { get; init; }
+    public Operation Operation { get; init; }
+
+    public CSG(Operation operation, Shape left, Shape right, Matrix? transform = null) : base(transform)
+    {
+        Left = left;
+        Right = right;
+        Operation = operation;
+    }
+
+    public override BoundingBox BoundsOf()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Intersections LocalIntersect(Ray localRay)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Vector LocalNormal(Point localPoint)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public enum Operation
+{
+    Union,
+    Intersection,
+    Difference
+}
