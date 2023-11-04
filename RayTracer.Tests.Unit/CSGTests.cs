a8ea71a52e123b61e4b84e4f9aaec702408d1c6e
mik-ke <mickster@duck.com>
2023-11-04 16:24:30 +0200

    CSG intersectionAllowed function.

--- RayTracer.Tests.Unit/CSGTests.cs
+++ RayTracer.Tests.Unit/CSGTests.cs
@@ -21,4 +21,70 @@ public class CSGTests
 		Assert.Equal(right, result.Right);
 		Assert.Equal(operation, result.Operation);
 	}
+
+	[Theory]
+	[InlineData(true, true, true, false)]
+	[InlineData(true, true, false, true)]
+	[InlineData(true, false, true, false)]
+	[InlineData(true, false, false, true)]
+	[InlineData(false, true, true, false)]
+	[InlineData(false, true, false, false)]
+	[InlineData(false, false, true, true)]
+	[InlineData(false, false, false, true)]
+	public void IntersectionAllowed_ShouldReturnCorrectResult_WhenUnionOperation(
+		bool leftHit, bool inLeft, bool inRight, bool expected)
+	{
+		// Arrange
+        Operation operation = Operation.Union;
+
+        // Act
+        var result = CSG.IntersectionAllowed(operation, leftHit, inLeft, inRight);
+
+        // Assert
+        Assert.Equal(expected, result);
+	}
+
+	[Theory]
+	[InlineData(true, true, true, true)]
+	[InlineData(true, true, false, false)]
+	[InlineData(true, false, true, true)]
+	[InlineData(true, false, false, false)]
+	[InlineData(false, true, true, true)]
+	[InlineData(false, true, false, true)]
+	[InlineData(false, false, true, false)]
+	[InlineData(false, false, false, false)]
+	public void IntersectionAllowed_ShouldReturnCorrectResult_WhenIntersectionOperation(
+		bool leftHit, bool inLeft, bool inRight, bool expected)
+	{
+        // Arrange
+        Operation operation = Operation.Intersection;
+
+        // Act
+        var result = CSG.IntersectionAllowed(operation, leftHit, inLeft, inRight);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+	[Theory]
+	[InlineData(true, true, true, false)]
+	[InlineData(true, true, false, true)]
+	[InlineData(true, false, true, false)]
+	[InlineData(true, false, false, true)]
+	[InlineData(false, true, true, true)]
+	[InlineData(false, true, false, true)]
+	[InlineData(false, false, true, false)]
+	[InlineData(false, false, false, false)]
+	public void IntersectionAllowed_ShouldReturnCorrectResult_WhenDifferenceOperation(
+		bool leftHit, bool inLeft, bool inRight, bool expected)
+	{
+        // Arrange
+        Operation operation = Operation.Difference;
+
+        // Act
+		var result = CSG.IntersectionAllowed(operation, leftHit, inLeft, inRight);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
 }
--- RayTracer/Shapes/CSG.cs
+++ RayTracer/Shapes/CSG.cs
@@ -32,6 +32,26 @@ public sealed class CSG : Shape
     {
         throw new NotImplementedException();
     }
+
+    /// <summary>
+    /// Defines whether an intersection is allowed based on the given parameters.
+    /// </summary>
+    /// <param name="operation">The CSG operation being evaluated.</param>
+    /// <param name="leftHit">True if the <see cref="Left"/> was hit and false if <see cref="Right"/> was hit.</param>
+    /// <param name="inLeft">True if the hit occurs inside <see cref="Left"/>.</param>
+    /// <param name="inRight">True if the hit occursinside <see cref="Right"/>.</param>
+    /// <exception cref="NotImplementedException">Thrown if the given <paramref name="operation"/> is not supported.</exception>
+    public static bool IntersectionAllowed(Operation operation, bool leftHit, bool inLeft, bool inRight)
+    {
+        if (operation == Operation.Union)
+            return (leftHit && !inRight) || (!leftHit && !inLeft);
+        else if (operation == Operation.Intersection)
+            return (leftHit && inRight) || (!leftHit && inLeft);
+        else if (operation == Operation.Difference)
+            return (leftHit && !inRight) || (!leftHit && inLeft);
+        else
+            throw new NotImplementedException();
+    }
 }
 
 public enum Operation
