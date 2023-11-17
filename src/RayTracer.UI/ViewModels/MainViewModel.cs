174960b07510221a33954f4c97172d1cbea79d81
mik-ke <mickster@duck.com>
2023-11-17 17:02:06 +0200

    More basic ui library functionality

--- src/RayTracer.UI/ViewModels/MainViewModel.cs
+++ src/RayTracer.UI/ViewModels/MainViewModel.cs
@@ -1,13 +1,38 @@
-﻿using RayTracer.Interfaces;
+﻿using System.Windows.Input;
+using RayTracer.Interfaces;
+using RayTracer.UI.MVVM;
 
 namespace RayTracer.UI.ViewModels;
 
 public class MainViewModel : BaseViewModel
 {
     private readonly IPpmWriter _ppmWriter;
+    
+    public ICommand AddShapeCommand { get; }
+    public ICommand AddLightCommand { get; }
+    public ICommand RenderCommand { get; }
 
     public MainViewModel(IPpmWriter ppmWriter)
     {
         _ppmWriter = ppmWriter;
+        
+        AddShapeCommand = new RelayCommand(AddShape); 
+        AddLightCommand = new RelayCommand(AddLight);
+        RenderCommand = new AsyncRelayCommand(RenderAsync);
+    }
+
+    private void AddShape(object? obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void AddLight(object? obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private Task RenderAsync(object? arg)
+    {
+        throw new NotImplementedException();
     }
 }
